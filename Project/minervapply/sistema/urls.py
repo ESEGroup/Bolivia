23031432ba132b11d8044a84878f15420b8eb1cf
juanmoli9 <juanmolimalax@gmail.com>
2016-11-15 18:49:33 -0200

    working all funcionalities

--- Project/minervapply/sistema/urls.py
+++ Project/minervapply/sistema/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url, include
-from . import views
+from sistema import views
 
 urlpatterns=[
-    url(r'^(?P<nome_professor>\w+)/$', views.professor_logado, name = 'professor_logado'),
-    url(r'^(?P<nome_professor>\w+)/formulario/$', views.formulario_vaga, name = 'formulario'),
+    url(r'^(?P<nome_professor>\w+)/$', views.professor_logado, name = 'professor-logado'),
+    #url(r'', views.escolha_professor, name='escolha-professor'),
+    url(r'^(?P<nome_professor>\w+)/add/$', views.Criar_Vaga.as_view(), name='adicionar-vaga'),
+    url(r'^(?P<nome_professor>\w+)/(?P<pk>[0-9]+)/$', views.Atualizar_Vaga.as_view(), name='atualizar-vaga'),
+    url(r'^(?P<nome_professor>\w+)/(?P<pk>[0-9]+)/delete/$', views.Apagar_Vaga.as_view(), name='apagar-vaga'),
 ]
--- Project/minervapply/sistema/views.py
+++ Project/minervapply/sistema/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse, Http404
 from .models import *
-
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.core.urlresolvers import reverse_lazy
+from .urls import *
 # Create your views here.
+
+"""def escolha_professor(request):
+    context = {'lista_professores' : Professor.objects.all()}
+    return render(request, 'sistema/escolha_professor.html', context)"""
+
+
 def professor_logado(request,nome_professor):
     try:
         professor = Professor.objects.get(nome = nome_professor)
@@ -12,5 +20,26 @@ def professor_logado(request,nome_professor):
     context = {'professor' : professor, 'lista_vagas' : lista_vagas}
     return render(request, 'sistema/professor_logado.html', context)
 
-def formulario_vaga(request,nome_professor):
-    return render(request,'sistema/ofertarVaga_br.html',{})
+
+class Criar_Vaga(CreateView):
+    model = Vaga
+    fields = ['titulo','remuneracao','local','prazo_de_aplicacao','tipo']
+    context_object_name = "lista_vagas_professor"
+    success_url = reverse_lazy('professor-logado', args = ['Ticiana'])
+    def get_professor(self):
+        return self.kwargs['nome_professor']
+
+
+
+class Atualizar_Vaga(UpdateView):
+    model = Vaga
+    fields = ['titulo','remuneracao','local','prazo_de_aplicacao','tipo']
+    success_url = reverse_lazy('professor-logado',args = ['Ticiana'])
+
+
+
+class Apagar_Vaga(DeleteView):
+    model = Vaga
+    success_url = reverse_lazy('professor-logado',args = ['Ticiana'])
+    def get_professor(self):
+        return self.kwargs['nome_professor']
